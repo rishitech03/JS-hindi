005d3ba655b7dd279cc72025f5b7921fe1c10c3a
Rishi Raj <rishi211099@gmail.com>
2025-07-15 13:56:20 +0000

    comparison done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0); // == works differently from conversion( <, >, <=, >=)
+console.log(null >= 0); // comparison converts null to a number with the value of 0       but == doesn't
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
