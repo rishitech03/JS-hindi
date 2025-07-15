3b3202282f5dfd69b79e79ff37f58a4fdb696c24
rishitech03 <rishi211099@gmail.com>
2025-07-15 12:50:17 +0000

    datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";  // treat all JS code as newer version
+
+// alert(3 + 3)  // we are using nodejs, not browser
+
+// cod readability should be high
+
+let name = "rishi"
+let age = 21
+
+// number => 2 to 2power53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => variable declared but not assigned any value
+// symbol => used to find uniqueness, mostly symbols are used in react and figma
+// object
+
+console.log(typeof undefined);  // type: undefined
+console.log(typeof null);       // type: object
+console.log(typeof Object);     // type: function
